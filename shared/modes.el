d1e8f5b3a8a889ae2eec4fa23855d1ef37d0ad65
jrun <jeremy.burks@gmail.com>
2011-04-24 09:21:58 -0500

    Add magit.

--- shared/modes.el
+++ shared/modes.el
@@ -6,3 +6,11 @@
 (load "shared/ruby")
 (load "shared/sgml")
 (load "shared/snippets")
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(vendor 'magit)
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")))
