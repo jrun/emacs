90923b28892f3719387e351f172a025fac154dc3
jrun <jeremy.burks@gmail.com>
2011-04-28 21:03:24 -0500

    rspec-mode for rspec 2

--- shared/ruby.el
+++ shared/ruby.el
@@ -1,20 +1,14 @@
-; rvm
 (vendor 'rvm)
 (rvm-use-default)
 
-; rinari
+(vendor 'rspec-mode)
+
 (vendor 'rinari)
 (setq rdinari-tags-file-name "TAGS")
 (add-hook 'rinari-minor-mode-hook
           (lambda ()
             (define-key rinari-minor-mode-map (kbd "A-r") 'rinari-test)))
 
-; rspec
-;;(vendor 'rspec-mode)
-;;(add-to-list 'load-path (concat dotfiles-dir "/vendor/rspec-mode"))
-;;(require 'rspec-mode)
-
-; ruby
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.sake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
@@ -25,6 +19,5 @@
 
 (add-to-list 'completion-ignored-extensions ".rbc")
 
-; treetop
 (vendor 'treetop-mode)
 (add-to-list 'auto-mode-alist '("\\.treetop\\'" . treetop-mode))
