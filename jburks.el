ede58dac2273906a88a1550cd1d8729ceed8df8e
jrun <jeremy.burks@gmail.com>
2011-04-28 20:40:04 -0500

    Lower font size, I'm not that old yet.

--- jburks.el
+++ jburks.el
@@ -4,6 +4,6 @@
 
 ;; Font
 ;;(set-default-font "-microsoft-consolas-medium-r-normal--0-0-0-0-m-0-iso8859-13")
-(set-default-font "-apple-Menlo-medium-normal-normal-*-13-*-*-*-m-0-fontset-auto3")
+(set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-fontset-auto3")
 
 (setq-default ispell-program-name "/usr/local/bin/aspell")
