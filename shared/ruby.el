204db5c1c63bc0e864a3592bed13e91ae8eb07c7
jrun <jeremy.burks@gmail.com>
2011-05-03 07:33:30 -0500

    Register a couple more ruby-mode file types.

--- shared/ruby.el
+++ shared/ruby.el
@@ -18,7 +18,9 @@
 (add-to-list 'auto-mode-alist '("\\.sake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("pryrc$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 
