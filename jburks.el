0138601b7aff2eb3e6bde1f7b8d80c1ca71a53f0
jrun <jeremy.burks@gmail.com>
2011-05-04 20:52:51 -0500

    Fix font.

--- jburks.el
+++ jburks.el
@@ -2,8 +2,5 @@
 ;;(setq mac-command-modifier 'meta)
 (setq ns-command-modifier 'meta)
 
-;; Font
-;;(set-default-font "-microsoft-consolas-medium-r-normal--0-0-0-0-m-0-iso8859-13")
-(set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-fontset-auto3")
-
+(set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
 (setq-default ispell-program-name "/usr/local/bin/aspell")
