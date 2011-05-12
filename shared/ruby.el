3b16aa9bf909fcc1dbd1e6a1dd5167cf7a21bf2b
jrun <jeremy.burks@gmail.com>
2011-05-11 20:36:20 -0500

    Inform ruby-mode about Guardfiles.

--- shared/ruby.el
+++ shared/ruby.el
@@ -23,6 +23,7 @@
 (add-to-list 'auto-mode-alist '("\\.sake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("pryrc$" . ruby-mode))
