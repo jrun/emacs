573d61d3a8d382f56537be59a6fa5baeb554b5f1
jrun <jeremy.burks@gmail.com>
2011-05-26 10:25:50 -0500

    Actually configure uniquify.

--- shared/global.el
+++ shared/global.el
@@ -37,3 +37,8 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (ansi-color-for-comint-mode-on)
+
+(setq uniquify-buffer-name-style 'reverse)
+(setq uniquify-separator "|")
+(setq uniquify-after-kill-buffer-p t) ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
