343c2bc989315e1347c35281559ca253c933508f
jrun <jeremy.burks@gmail.com>
2011-05-27 14:57:19 -0500

    Tweak the tangotango highlight so it looks okay in anything.

--- vendor/color-theme-tangotango.el
+++ vendor/color-theme-tangotango.el
@@ -1,4 +1,4 @@
-;;; Emacs Color theme based on the Tango Palette colors. 
+;;; Emacs Color theme based on the Tango Palette colors.
 ;;; First derived from color-theme-tango.el,  created by danranx@gmail.com :
 ;;; http://www.emacswiki.org/emacs/color-theme-tango.el
 
@@ -56,7 +56,7 @@
       (ibuffer-read-only-buffer-face . font-lock-type-face)
       (ibuffer-special-buffer-face . font-lock-keyword-face)
       (ibuffer-title-face . font-lock-type-face))
-     (highlight ((t (:background "brown4" :foreground nil))))
+     (highlight ((t (:background "#222222" :foreground "orange"))))
      (border ((t (:background "#888a85"))))
      (fringe ((t (:background "grey10"))))
      (mode-line ((t (:foreground "#bbbbbc" :background "#222222" :box (:line-width 1 :color nil :style released-button)))))
@@ -111,7 +111,7 @@
      (custom-button-pressed  ((t (:inherit 'custom-button :box (:style pressed-button)))))
      (custom-button-mouse-pressed-unraised  ((t (:inherit 'custom-button-unraised :background "grey60"))))
      (custom-documentation ((t (:italic t))))
-     (message-cited-text ((t (:foreground "#edd400")))) 
+     (message-cited-text ((t (:foreground "#edd400"))))
      (gnus-cite-face-1 ((t (:foreground "#ad7fa8"))))
      (gnus-cite-face-2 ((t (:foreground "sienna4"))))
      (gnus-cite-face-3 ((t (:foreground "khaki4"))))
