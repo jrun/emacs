0bc3603a8112321349722d8f760998269a8bc10a
jrun <jeremy.burks@gmail.com>
2011-05-27 17:15:52 -0500

    Adjust font.

--- jburks.el
+++ jburks.el
@@ -2,5 +2,6 @@
 ;;(setq mac-command-modifier 'meta)
 (setq ns-command-modifier 'meta)
 
-(set-default-font "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(set-default-font "Consolas-12")
+
 (setq-default ispell-program-name "/usr/local/bin/aspell")
