c8bf6c39e7b72b7fe8c9be285c622cf45da09477
jrun <jeremy.burks@gmail.com>
2011-05-27 07:39:14 -0500

    Remove modes I won't be using anymore.

--- jtburks.el
+++ jtburks.el
@@ -1,8 +1,1 @@
-;; Font
-(set-default-font "-microsoft-Consolas-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-
-(load "shared/groovy")
-(load "shared/velocity")
-
-
-
+(set-default-font "Inconsolata-11")
