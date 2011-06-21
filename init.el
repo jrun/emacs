69c23cb145d0601d3c224577782dfae68d14435e
jburks <jeremy.burks@gmail.com>
2011-06-20 21:22:45 -0500

    Tell emacs about /usr/local/bin.

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
+(push "/usr/local/bin" exec-path)
+
 (setq emacsd "~/.emacs.d/")
 
 (require 'ansi-color)
@@ -46,3 +48,4 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  )
+(put 'erase-buffer 'disabled nil)
