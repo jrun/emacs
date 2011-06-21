20b8db6e7babb80aed5656a471143299b87eb023
jburks <jeremy.burks@gmail.com>
2011-06-20 21:22:14 -0500

    Back to vibrant ink.

--- shared/color-theme.el
+++ shared/color-theme.el
@@ -1,5 +1,7 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 
+(load-file (concat emacsd "vendor/color-theme-vibrant-ink.el"))
 (load-file (concat emacsd "vendor/color-theme-tangotango.el"))
-(color-theme-tangotango)
+;(color-theme-tangotango)
+(color-theme-vibrant-ink)
