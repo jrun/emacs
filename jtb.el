92a386afbe2cdbd3f031d1cea4b49d3bc1ded48b
Jeremy <jeremy.burks@gmail.com>
2011-08-18 15:56:48 -0500

    Add additional username.

---
+++ jtb.el
@@ -0,0 +1,7 @@
+;; override the deafult aquamacs meta key to command
+;;(setq mac-command-modifier 'meta)
+(setq ns-command-modifier 'meta)
+
+(set-default-font "Menlo-10")
+
+(setq-default ispell-program-name "/usr/local/bin/aspell")
