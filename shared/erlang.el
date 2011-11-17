604e96f44cc3a0de1a1aa9fa4a97582472d6ae0b
jeremy <jeremy.burks@gmail.com>
2011-11-17 14:00:19 -0600

    Updated erlang which broke paths.

--- shared/erlang.el
+++ shared/erlang.el
@@ -1,9 +1,9 @@
 ;; http://www.erlang.org/doc/man/erlang.el.html
 
 ;; This is needed for Erlang mode setup
-(setq load-path (cons "/usr/local/Cellar/erlang/R14B03/lib/erlang/lib/tools-2.6.6.4/emacs" load-path))
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R14B03")
-(setq exec-path (cons "/usr/local/Cellar/erlang/R14B03/bin" exec-path))
+(setq load-path (cons "/usr/local/Cellar/erlang/R14B04/lib/erlang/lib/tools-2.6.6.5/emacs" load-path))
+(setq erlang-root-dir "/usr/local/Cellar/erlang/R14B04")
+(setq exec-path (cons "/usr/local/Cellar/erlang/R14B04/bin" exec-path))
 (require 'erlang-start)
 
 ;; Tell Emacs not to wait the usual 60 seconds for an Erlang prompt
