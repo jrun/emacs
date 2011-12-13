720f43f87745ef944acd25a44cbdf830b1a3057c
jeremy <jeremy.burks@gmail.com>
2011-12-13 09:02:16 -0600

    Yuk.

--- shared/groovy.el
+++
@@ -1,7 +0,0 @@
-(autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
-
-(add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
-(add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
-
-;;(add-to-list 'load-path (concat dotfiles-dir "/vendor/groovy-mode.el"))
-(require 'groovy-mode)
