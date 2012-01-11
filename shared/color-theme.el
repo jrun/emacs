cc9f4f587b40d112cc82ff38ca00d82dda4e2908
jeremy <jeremy.burks@gmail.com>
2012-01-11 12:49:54 -0600

    Add thor color-theme.

--- shared/color-theme.el
+++ shared/color-theme.el
@@ -2,6 +2,7 @@
 (setq color-theme-is-global t)
 
 (load-file (concat emacsd "vendor/color-theme-vibrant-ink.el"))
-(load-file (concat emacsd "vendor/color-theme-tangotango.el"))
+(load-file (concat emacsd "vendor/color-theme-thor.el"))
+
 ;(color-theme-tangotango)
 (color-theme-vibrant-ink)
