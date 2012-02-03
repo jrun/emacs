7bf889f1fe2e55e50bb4e6d912fbb86a3b88a105
jeremy <jeremy.burks@gmail.com>
2012-02-03 12:33:42 -0600

    Cleanup color-theme loading.

--- shared/color-theme.el
+++ shared/color-theme.el
@@ -1,8 +1,4 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
-
 (load-file (concat emacsd "vendor/color-theme-vibrant-ink.el"))
-(load-file (concat emacsd "vendor/color-theme-thor.el"))
-
-;(color-theme-tangotango)
 (color-theme-vibrant-ink)
