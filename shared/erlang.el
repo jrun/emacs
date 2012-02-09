e682473bb3aaa4f98489287f4fcc05184f8e0824
jeremy <jeremy.burks@gmail.com>
2012-02-08 21:46:54 -0600

    Don't try to configure erlang if it appears to be not installed.

--- shared/erlang.el
+++ shared/erlang.el
@@ -1,12 +1,16 @@
 ;; http://www.erlang.org/doc/man/erlang.el.html
 
-;; This is needed for Erlang mode setup
-(setq load-path (cons "/usr/local/Cellar/erlang/R14B04/lib/erlang/lib/tools-2.6.6.5/emacs" load-path))
 (setq erlang-root-dir "/usr/local/Cellar/erlang/R14B04")
-(setq exec-path (cons "/usr/local/Cellar/erlang/R14B04/bin" exec-path))
-(require 'erlang-start)
 
-;; Tell Emacs not to wait the usual 60 seconds for an Erlang prompt
-(defvar inferior-erlang-prompt-timeout t)
+(if (file-exists-p erlang-root-dir)
+    (progn
+     ;; This is needed for Erlang mode setup
 
-(add-to-list 'auto-mode-alist '("rabbitmq.config" . treetop-mode))
+      (setq load-path (cons (concat erlang-root-dir "/lib/erlang/lib/tools-2.6.6.5/emacs") load-path))
+      (setq exec-path (cons (concat erlang-root-dir "/bin") exec-path))
+      (require 'erlang-start)
+
+      ;; Tell Emacs not to wait the usual 60 seconds for an Erlang prompt
+      (defvar inferior-erlang-prompt-timeout t)
+
+      (add-to-list 'auto-mode-alist '("rabbitmq.config" . treetop-mode))))
