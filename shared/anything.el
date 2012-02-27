7afa94a1d81e32527a70c4465c2c9f090f52dd33
jeremy <jeremy.burks@gmail.com>
2012-02-27 11:08:45 -0600

    Update anything and add anything etags navigation.

--- shared/anything.el
+++ shared/anything.el
@@ -1,7 +1,7 @@
 (require 'anything)
 (require 'anything-config)
 (require 'anything-match-plugin)
-(require 'anything-rails)
+(require 'anything-etags)
 
 (defun my-anything ()
   "My Anything command"
@@ -9,10 +9,10 @@
   (anything-other-buffer
    '(anything-c-source-fixme
      anything-c-source-buffers+
-     anything-c-source-rails-project-files
      anything-c-source-files-in-current-dir+
      anything-c-source-file-name-history)
    "*my-anything*"))
 
 (global-set-key (kbd "M-X") 'my-anything)
 (global-set-key [(meta f11)] 'anything-resume)
+(global-set-key (kbd "M-.") 'anything-etags-select-from-here)
