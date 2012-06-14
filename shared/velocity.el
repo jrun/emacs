9cfe917f55b8c7c8254f49be9df26de4769ef336
Jeremy <jtb@storm.local>
2012-06-14 10:22:53 -0500

    Remove unused velocity.el.

--- shared/velocity.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'auto-mode-alist '("\.vm$" . html-mode))
-(autoload 'turn-on-vtl-mode "vtl" nil t)
-(add-hook 'html-mode-hook 'turn-on-vtl-mode)
-;;(add-hook 'xml-mode-hook 'turn-on-vtl-mode t t)
-;;(add-hook 'text-mode-hook 'turn-on-vtl-mode t t)
-
