32f29ed24d97f188a99bbd4cbe75a72e2b0ba1b6
Jeremy <jeremy@zomgwat.com>
2012-11-09 15:28:03 -0600

    coffee-script

--- shared/modes.el
+++ shared/modes.el
@@ -79,5 +79,19 @@
 ;; dtrace
 (autoload 'd-mode "d-mode" () t)
 (add-to-list 'auto-mode-alist '("\\.d\\'" . d-mode))
+;; coffee
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+
+  ;; CoffeeScript uses two spaces.
+  (make-local-variable 'tab-width)
+  (set 'tab-width 2))
+
+(add-hook 'coffee-mode-hook 'coffee-custom)
++
 ;; go
 (vendor 'go-mode-load)
