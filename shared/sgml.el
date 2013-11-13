0c66ef354e15f276ccc02ef278757f7884e52415
Jeremy Burks <jeremy@zomgwat.com>
2013-11-12 22:53:10 -0600

    Add web-mode.el.

--- shared/sgml.el
+++ shared/sgml.el
@@ -1,3 +1,22 @@
-(require 'zencoding-mode)
-(add-hook 'sgml-mode-hook 'zencoding-mode)
-(setq-default sgml-basic-offset 2)
+;; (require 'zencoding-mode)
+;; (add-hook 'sgml-mode-hook 'zencoding-mode)
+;; (setq-default sgml-basic-offset 2)
+
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2)
+  (setq web-mode-style-padding 2)
+  (setq web-mode-script-padding 2)
+  (setq web-mode-enable-current-element-highlight t)
+)
+
+(add-hook 'web-mode-hook 'web-mode-hook)
