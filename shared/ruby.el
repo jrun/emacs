7ece7cd085957779eb1de8df0456f76ea3f3ea26
Jeremy Burks <jeremy@zomgwat.com>
2014-05-09 16:03:27 -0500

    Update ruby.
    Also missed the bit about rhtml. To lazy to amend that commit.

--- shared/ruby.el
+++ shared/ruby.el
@@ -1,7 +1,7 @@
 (add-to-list 'load-path (concat emacsd "vendor/enhanced-ruby-mode"))
 
 ;; doesn't seem to work with ruby 2.0
-(setq enh-ruby-program "~/.rbenv/versions/ruby-2.0.0-p247/bin/ruby")
+(setq enh-ruby-program "~/.rbenv/versions/ruby-2.1.2/bin/ruby")
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
 
@@ -18,11 +18,6 @@
 (add-to-list 'auto-mode-alist '("pryrc$" . enh-ruby-mode))
 
 (add-to-list 'completion-ignored-extensions ".rbc")
-
-;; rhtml
-(add-to-list 'load-path (concat emacsd "vendor/rhtml-mode"))
-(vendor 'rhtml-mode)
-(add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
 
 ;; treetop
 (vendor 'treetop-mode)
