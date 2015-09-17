53727b348363377d694245cbf713600be2b4b89b
Jeremy Burks <jeremy@zomgwat.com>
2015-09-17 13:16:21 -0500

    Bump Erlang stuff.

--- shared/erlang.el
+++ shared/erlang.el
@@ -1,16 +1,14 @@
 ;; http://www.erlang.org/doc/man/erlang.el.html
 
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R14B04")
+(setq erlang-root-dir "/usr/local/opt/erlang")
 
 (if (file-exists-p erlang-root-dir)
     (progn
-     ;; This is needed for Erlang mode setup
+      ;; This is needed for Erlang mode setup
 
-      (setq load-path (cons (concat erlang-root-dir "/lib/erlang/lib/tools-2.6.6.5/emacs") load-path))
+      (setq load-path (cons (concat erlang-root-dir "/lib/erlang/lib/tools-2.8/emacs") load-path))
       (setq exec-path (cons (concat erlang-root-dir "/bin") exec-path))
       ;;(require 'erlang-start)
 
       ;; Tell Emacs not to wait the usual 60 seconds for an Erlang prompt
-      (defvar inferior-erlang-prompt-timeout t)
-
-      (add-to-list 'auto-mode-alist '("rabbitmq.config" . treetop-mode))))
+      (defvar inferior-erlang-prompt-timeout t)))
