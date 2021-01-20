1a25e2eaaa0faab6ed460ee02e19c475a517510e
Jeremy Burks <jeremy.burks@sikich.com>
2021-01-20 14:17:23 -0600

    A Python thing
    I don't remember why this is here but I'm assuming it's for a
    reason...

---
+++ shared/python.el
@@ -0,0 +1,9 @@
+(with-eval-after-load 'python
+  (defun python-shell-completion-native-try ()
+    "Return non-nil if can trigger native completion."
+    (let ((python-shell-completion-native-enable t)
+          (python-shell-completion-native-output-timeout
+           python-shell-completion-native-try-output-timeout))
+      (python-shell-completion-native-get-completions
+       (get-buffer-process (current-buffer))
+       nil "_"))))
